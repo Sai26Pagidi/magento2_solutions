9c362d61eeefd0c31fce4d924d58574620d334d6
sai <saiteja.pagidi@gmail.com>
2022-05-06 08:38:53 +0530

    ui component actions is commited

---
+++ app/code/Teja/Blog/Ui/Component/Listing/Grid/Column/Delete.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Teja\Blog\Ui\Component\Listing\Grid\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class Actions
+ */
+class Delete extends Column
+{
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                // here we can also use the data from $item to configure some parameters of an action URL
+                $item[$this->getData('name')] = [
+                    'delete' => [
+                        'href' => '/delete',
+                        'label' => __('Delete')
+                    ],
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
